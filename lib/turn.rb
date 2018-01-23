cf52f2cc7060809e06222cf4f7acd6265031b36c
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-23 05:57:41 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,40 @@
+def valid_move?(board, index)
+  isValid = false
+  if !position_taken?(board, index) && index.between?(0,8)
+    !isValid
+  else
+    isValid
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, index)
+  isHere = false
+  if board[index] == " " || board[index] == ""
+    isHere
+  elsif board[index] == nil
+    isHere
+  else
+    !isHere
+  end
+end
+
+def display_board(board) #Imprime el estado actual del juego
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+
+def input_to_index(cadena)
+  index = cadena.to_i
+  index -= 1
+end
+
+def move(board, index, caracter = "X")
+  board[index] = caracter
+end
